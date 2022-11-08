ad82ecc9ff59b947b1aec99f6577e365dc242289
joydharsamrat <joydharsamrat@gmail.com>
2022-11-09 03:00:08 +0600

    my reviews page created

--- src/pages/MyReviews/MyReviews.js
+++ src/pages/MyReviews/MyReviews.js
@@ -1,10 +1,21 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { authContext } from '../../contexts/AuthProvider/AuthProvider';
+import ReviewCard from './ReviewCard';
 
 const MyReviews = () => {
-    const [reviews, setReviews] = useState([])
+    const { user } = useContext(authContext);
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/reviews?email=${user?.email}`)
+            .then(res => res.json())
+            .then(data => setReviews(data))
+    }, [user?.email, reviews])
     return (
         <div>
-
+            {
+                reviews.map(review => <ReviewCard key={review._id} review={review}></ReviewCard>)
+            }
         </div>
     );
 };
